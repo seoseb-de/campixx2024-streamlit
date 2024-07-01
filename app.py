1a36761c792c067ca71aec231b03ae5014e8f156
Seoseb <79011155+seoseb-de@users.noreply.github.com>
2024-07-01 13:09:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.set_page_config(
 with st.sidebar:
     
     st.title('Sidebar Stuff')
-    st.caption('Demo-Apps und Ressourcen des Campixxx Vortrags vom 14.06.2024')
+    st.markdown('Demo-Apps und Ressourcen des Campixxx Vortrags [https://app.campixx.de/item/konfetti-und-ballons-statt-cod](https://app.campixx.de/item/konfetti-und-ballons-statt-cod) vom 14.06.2024')
     st.markdown('_fiddled by [seoseb](https://www.seoseb.de) | [@seoseb](https://seocommunity.social/@seoseb)_')
 
 #############
